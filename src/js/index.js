18bebaf68e589d2232f3e6d64d4431e84c9a52e0
Katsunuma Maki <khats@blue335.jp>
2018-10-14 22:07:26 +0900

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free-solid';
+import '../sass/index.sass';
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                            importLoaders: 2
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/html/*.html', flatten: true },
+            { from: 'src/images/*', to: 'images/', flatten: true }
+        ])
+    ]
+};
