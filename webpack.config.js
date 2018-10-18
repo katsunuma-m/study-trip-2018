2d23f8933307276693b5afea0d2cb8f9f5a03e5e
Katsunuma Maki <Katsunuma.Maki@s50.toshiba-sol.co.jp>
2018-10-18 13:44:24 +0900

    HTMLインクルードの仕組みを作成

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 const path = require('path');
+var fs = require("fs");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+includehtml = (content, srcpath) => content.toString().split(/\r\n/).map(l => {
+    const re = /^(\s+)<!--\s+include\s+(\S+)\s+-->/;
+    const result = re.exec(l);
+    if (!result) return l;
+    const indent = result[1];
+    const filename = path.join(path.dirname(srcpath), result[2]);
+    const data = fs.readFileSync(filename);
+    return data.toString().split(/\r\n/).map(l => {
+        return l ? indent + l : l;
+    }).join('\r\n');
+}).join('\r\n');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -33,7 +46,12 @@ module.exports = {
     },
     plugins: [
         new CopyWebpackPlugin([
-            { from: 'src/html/*.html', flatten: true },
+            {
+                from: 'src/html/*.html',
+                ignore: ['*.part.html', '*.partial.html'],
+                flatten: true,
+                transform: includehtml
+            },
             { from: 'src/images/*', to: 'images/', flatten: true }
         ])
     ]
